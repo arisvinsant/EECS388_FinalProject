b62e2c7f761e83d60207b12fdfc4e98de68a0b49
beauhodes <49450040+beauhodes@users.noreply.github.com>
2019-04-16 10:59:47 -0500

    Update recordVoice.py

--- recordVoice.py
+++ recordVoice.py
@@ -5,9 +5,9 @@ form_1 = pyaudio.paInt16 # 16-bit resolution
 chans = 1 # 1 channel
 samp_rate = 44100 # 44.1kHz sampling rate
 chunk = 4096 # 2^12 samples for buffer
-record_secs = 3 # seconds to record
-dev_index = 2 # device index found by p.get_device_info_by_index(ii)
-wav_output_filename = 'test1.wav' # name of .wav file
+record_secs = 10 # seconds to record
+dev_index = 2 # device index found by p.get_device_info_by_index(ii) (THIS MAY CHANGE DEPENDING ON THE PI)
+wav_output_filename = 'TeamTwelveAudio.wav' # name of .wav file 
 
 audio = pyaudio.PyAudio() # create pyaudio instantiation
 
